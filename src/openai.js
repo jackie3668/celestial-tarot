e6a09562b33641034bf3513d69fb0cdba8cb3858
jackie <js6952@nyu.edu>
2023-10-13 00:54:28 -0400

    Connected API Server

--- src/openai.js
+++ src/openai.js
@@ -1,27 +1,8 @@
-export async function sendMsgToOpenAI(userMessage) {
-  try {
-    console.log('Before fetch:', userMessage);
-
-    const responseFromAPI = await fetch('http://ec2-3-21-92-197.us-east-2.compute.amazonaws.com/api/sendMsgToOpenAI', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ userMessage }),
-    });
-
-    console.log('After fetch:', responseFromAPI);
-
-    if (responseFromAPI.ok) {
-      const data = await responseFromAPI.json();
-      console.log('Data from API:', data);
-      return data;
-    } else {
-      console.error('Error making a request to the backend API');
-      throw new Error('Error making a request to the backend API');
-    }
-  } catch (error) {
-    console.error('Error in sendMsgToOpenAI:', error);
-    throw error;
-  }
-}
+const response = await fetch(`${apiUrl}/sendMsgToOpenAI`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userMessage }),
+  });
+  
