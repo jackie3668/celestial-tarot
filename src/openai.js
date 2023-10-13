a57fb554fcd4cf7e4bf316595924abfead38e09c
jackie <js6952@nyu.edu>
2023-10-13 15:52:06 -0400

    Deleted Openai

--- src/openai.js
+++
@@ -1,8 +0,0 @@
-const response = await fetch(`${apiUrl}/sendMsgToOpenAI`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ userMessage }),
-  });
-  
