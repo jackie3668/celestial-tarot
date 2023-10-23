899739ce545f799127c225436f9aac39b6dc840c
Jackie <actions@github.com>
2023-10-22 22:25:15 -0400

    Question Page

--- src/pages/Question/Question.js
+++ src/pages/Question/Question.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Question.css';
 
 function Question() {
   return (
-    <div>
-      <div className="question-container">
-        <div className="question-input-container">
-          <div className="question-input-back">
-            <div className="question-input-front">
-              <input type="text" placeholder='Ask a question'/>
-            </div>
+    <div className="question-container">
+      <div className="question-input-container">
+        <div className="question-input-back">
+          <div className="question-input-front">
+            <input className='header-3' type="text" placeholder='Ask a question'/>
           </div>
         </div>
         <Link to="/shuffle">
-        <button className='question-button'>Ready</button>
+          <button className='question-button'>Ready</button>
         </Link>
       </div>
-     
+
+
+      <div className="cloud-container">
+        <div className="cloud1 moving-image-container">
+          <img className="cloud1-img still-image" src={require('../../assets/cloud1.png')} alt="cloud 1" />
+        </div>
+        <div className="cloud2">
+          {/* <img className="cloud2-img still-image" src={require('../../assets/cloud2.png')} alt="cloud 2" /> */}
+        </div>
+        <div className="cloud3">
+          <img className="cloud3-img still-image" src={require('../../assets/cloud3.png')} alt="cloud 3" />
+        </div>
+      </div>
     </div>
   );
 }
