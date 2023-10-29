c0241b2b8075f6af281136af8683c412eb5480b0
Jackie <actions@github.com>
2023-10-29 17:36:27 -0400

    Finished Shuffle

--- src/containers/Shuffle/Shuffle.js
+++ src/containers/Shuffle/Shuffle.js
@@ -12,37 +12,44 @@ const [buttonVisibility, setButtonVisibility] = useState(false);
 const handleCardClick = (e) => {
   const card = e.target;
   const selectedCards = document.querySelectorAll('.selected');
-  
+
   if (selectedCards.length < maxCards) {
     card.classList.add('fadeOutDown');
 
     setTimeout(() => {
       card.classList.remove('fadeOutDown');
       card.classList.add('selected');
-      card.classList.remove('shuffle-card')
+      card.classList.remove('shuffle-card');
 
       const selectedContainer = document.querySelector('.selected-container');
-      
+
       const index = selectedCards.length;
-      
+
       const divToAppendTo = selectedContainer.querySelector(
         `.card:nth-child(${index + 1}) .card-front`
       );
       divToAppendTo.appendChild(card);
-    }, 700);
-    
+
+      setTimeout(() => {
+        const parentCard = card.closest('.card');
+        parentCard.classList.add('flipped');
+      }, 500);
+    }, 500);
+
     if (selectedCards.length === (maxCards - 1)) {
       setButtonVisibility(true);
+      const shuffleCards = document.querySelectorAll('.shuffle-card');
+      shuffleCards.forEach((shuffleCard) => {
+        shuffleCard.style.cursor = 'not-allowed';
+        shuffleCard.classList.remove('hover-effect-class');
+        
+      });
+
     }
   }
 };
 
 const divWidth = (maxCards * 112) + (maxCards - 1) * 15
-
-const handleCardFlip = (e) => {
-  console.log('Clicked Element:', e.currentTarget); 
-  e.currentTarget.classList.add('flipped')
-}
 
   return (
     <div className='shuffle-container'>
@@ -51,7 +58,7 @@ const handleCardFlip = (e) => {
         {Array.from({ length: 30 }).map((_, index) => (
           <img
             key={index}
-            className='shuffle-card'
+            className='shuffle-card hover-effect-class'
             onClick={handleCardClick}
             src={require('../../assets/design-1/0.png')}
             alt="tarot image"
@@ -60,10 +67,10 @@ const handleCardFlip = (e) => {
       </div>
 
       <div className="selected-container" style={{ width: `${divWidth}px` }}>
-        <div className="card" onClick={handleCardFlip}>
+        <div className="card">
           <div className="card-inner">
-            <div className="card-front">Front of the card</div>
-            <div className="card-back">Back of the card
+            <div className="card-front"></div>
+            <div className="card-back">
               <img
               src={require('../../assets/design-1/1.png')}
               alt="tarot image"
@@ -74,20 +81,20 @@ const handleCardFlip = (e) => {
         
         <div className="card">
           <div className="card-inner">
-            <div className="card-front">Front of the card</div>
-            <div className="card-back">Back of the card
+            <div className="card-front"></div>
+            <div className="card-back">
               <img
               src={require('../../assets/design-1/1.png')}
               alt="tarot image"
               />
-          </div>
+            </div>
           </div>
         </div>
         
         <div className="card">
           <div className="card-inner">
-            <div className="card-front">Front of the card</div>
-            <div className="card-back">Back of the card
+            <div className="card-front"></div>
+            <div className="card-back">
               <img
               src={require('../../assets/design-1/1.png')}
               alt="tarot image"
