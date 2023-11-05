b5c93cf350c21eb0e586141386ef47778046443f
Jackie <actions@github.com>
2023-11-05 15:53:20 -0500

    Responsive Design

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,13 +3,17 @@ import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+  const handleClick = () => {
+
+  }
   return (
     <div className="navbar-container">
       <a href="/celestial-tarot" className="link">
         <div className="logo header-1">Celestial</div>
       </a>
 
-      <nav>
+      {/* <div className="menu" onClick={handleClick}>=</div> */}
+      <nav className='mobile-invisible'>
         <ul className="nav-links">
           <Link to="/" className='link'><li className='text'>Home</li></Link>
           <Link to="/reading" className='link'><li className='text'>Get A Reading</li></Link>
